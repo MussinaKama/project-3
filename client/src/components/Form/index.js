e4625e7c9f0a3bf15903c878eff110d16f308b23
kamshat <mussinak777@gmail.com>
2020-03-12 16:20:16 -0600

    added animation to form, navlinks

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -15,13 +15,13 @@ import CityCard from "../Card/index";
 import Datepicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { FormWrapper } from "../../styles";
+import { motion } from "framer-motion";
 
 const CityForm = () => {
   const [data, setData] = useState({
     continentsList,
     countriesList
   });
-
   const [userInput, setUserInput] = useState({
     id: Math.floor(Math.random() * 100),
     city: "",
@@ -32,11 +32,9 @@ const CityForm = () => {
     events: [],
     image: ""
   });
-
   const [userDate, setUserDate] = useState({
     startDate: new Date()
   });
-
   const [savedCard, setSavedCard] = useState([]);
 
   useEffect(() => {
@@ -48,27 +46,30 @@ const CityForm = () => {
   }, []);
 
   const handleDateChange = date => {
-    setUserDate({ startDate: date});
+    setUserDate({ startDate: date });
   };
 
   const handleSearch = e => {
     const value = e.target.value;
     const filteredCountries = continentsList.filter(c => {
       let newValue = Object.values(c)
-      .join("")
-      .toLowerCase()
+        .join("")
+        .toLowerCase();
       return newValue.indexOf(value.toLowerCase()) !== -1;
-    })
+    });
+    setData({ countriesList: filteredCountries });
+  };
 
-    setData({countriesList: filteredCountries})
-  }
-
-  const formatDate = (date) => {
-    let fDate = new Date(date)
-    let sDate = fDate.getFullYear() + '/'+(fDate.getMonth() + 1) +'/'+fDate.getDate()
+  const formatDate = date => {
+    let fDate = new Date(date);
+    let sDate =
+      fDate.getFullYear() +
+      "/" +
+      (fDate.getMonth() + 1) +
+      "/" +
+      fDate.getDate();
     return sDate;
-
-  }
+  };
   const loadCityCards = () => {
     API.getCityCards()
       .then(res => setSavedCard(res.data))
@@ -93,21 +94,21 @@ const CityForm = () => {
       city: userInput.city,
       date: userDate.startDate,
       continent: userInput.continent,
-      country: userInput.activities,
+      country: userInput.country,
       activities: userInput.activities,
       restaurant: userInput.restaurant,
       events: userInput.events,
       image: userInput.image
     })
       .then(res => loadCityCards())
-      .catch(err => console.log(err)); 
+      .catch(err => console.log(err));
   };
 
   return (
     <>
       <Col>
         {savedCard.length ? (
-          <Container>
+          <Container style={{ marginLeft: 10 }}>
             {savedCard.map(user => (
               <CityCard
                 key={user.id}
@@ -134,78 +135,97 @@ const CityForm = () => {
           style={{ display: "block" }}
         >
           <FormWrapper>
-            <Form className="form">
-              <FormGroup>
-                <Label for="city">What city you want to visit next?</Label>
-                <Input
-                  type="text"
-                  id="city"
-                  onChange={handleInputChange}
-                  name="city"
-                />
-              </FormGroup>
-              <Datepicker
-                selected={userDate.startDate}
-                onChange={handleDateChange}
-                id="date"
-              ></Datepicker>
-              <FormGroup>
-                <Label for="continent">Choose continent</Label>
-                <select onChange={handleInputChange} name="continent" id="continent">
-                  {data.continentsList.map(c => (
-                    <option key={c.value}>{c.value}</option>
-                  ))}
-                </select>
-              </FormGroup>
-              <FormGroup>
-                <Label for="country">Choose country</Label>
-                <select onChange={handleInputChange}  onSearch={handleSearch} name="country" id="country">
-                  {data.countriesList.map(country => (
-                    <option key={country.name}>{country.name}</option>
-                  ))}
-                </select>
-              </FormGroup>
-              <FormGroup>
-                <Label for="restaurant">Restaurants</Label>
-                <Input
-                  type="text"
-                  id="restaurant"
-                  onChange={handleInputChange}
-                  name="restaurant"
-                  placeholder="Insert using comma"
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="activity">Activities</Label>
-                <Input
-                  type="text"
-                  id="activity"
-                  onChange={handleInputChange}
-                  name="activities"
-                  placeholder="Insert using comma"
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="event">Events</Label>
-                <Input
-                  type="text"
-                  id="event"
-                  onChange={handleInputChange}
-                  name="events"
-                  placeholder="Insert using comma"
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="image">Insert URL of image associated with this city/country</Label>
-                <Input
-                  type="text"
-                  id="image"
-                  onChange={handleInputChange}
-                  name="image"
-                />
-              </FormGroup>
-              <Button onClick={handleFormSubmit}>Submit</Button>
-            </Form>
+            <motion.div drag>
+              <Form className="form">
+                <FormGroup>
+                  <Label for="city">What city do you want to visit next?</Label>
+                  <Input
+                    type="text"
+                    id="city"
+                    onChange={handleInputChange}
+                    name="city"
+                  />
+                </FormGroup>
+                <Datepicker
+                  selected={userDate.startDate}
+                  onChange={handleDateChange}
+                  id="date"
+                ></Datepicker>
+                <FormGroup>
+                  <Label for="continent">Choose continent</Label>
+                  <select
+                    onChange={handleInputChange}
+                    name="continent"
+                    id="continent"
+                  >
+                    {data.continentsList.map(c => (
+                      <option key={c.value}>{c.value}</option>
+                    ))}
+                  </select>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="country">Choose country</Label>
+                  <select
+                    onChange={handleInputChange}
+                    onSearch={handleSearch}
+                    name="country"
+                    id="country"
+                  >
+                    {data.countriesList.map(country => (
+                      <option key={country.name}>{country.name}</option>
+                    ))}
+                  </select>
+                </FormGroup>
+                <FormGroup>
+                  <Label for="restaurant">
+                    Insert restaurants where you can taste some unusual food
+                  </Label>
+                  <Input
+                    type="text"
+                    id="restaurant"
+                    onChange={handleInputChange}
+                    name="restaurant"
+                    placeholder="Insert using comma"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="activity">
+                    Want to get some adrenaline? Insert "must do" activities{" "}
+                  </Label>
+                  <Input
+                    type="text"
+                    id="activity"
+                    onChange={handleInputChange}
+                    name="activities"
+                    placeholder="Insert using comma"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="event">
+                    Want to attend interesting events, festivals?
+                  </Label>
+                  <Input
+                    type="text"
+                    id="event"
+                    onChange={handleInputChange}
+                    name="events"
+                    placeholder="Insert using comma"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="image">
+                    Insert URL of image associated with this city/country
+                  </Label>
+                  <Input
+                    type="text"
+                    id="image"
+                    onChange={handleInputChange}
+                    name="image"
+                  />
+                </FormGroup>
+                <Button onClick={handleFormSubmit}>Submit</Button>
+              </Form>
+            </motion.div>
           </FormWrapper>
         </Col>
       </Container>
--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,22 +1,57 @@
 import React from "react";
-import { NavLink} from "reactstrap";
-import {NavWrapper} from "../../styles";
+import { NavLink } from "reactstrap";
+import { NavWrapper } from "../../styles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 import { faSignInAlt } from "@fortawesome/free-solid-svg-icons";
 import { faGlobeAmericas } from "@fortawesome/free-solid-svg-icons";
-
-
+import { motion } from "framer-motion";
 
 const TopNavbar = () => {
   return (
-        <NavWrapper className="flex-column">
-          <NavLink className="links" href="/" style={{fontSize:"35px", fontFamily:'Baloo Tammudu',}}> <FontAwesomeIcon icon={faHome} />About</NavLink>
-          <NavLink className="links" href="/profile" style={{fontSize:"35px", fontFamily:'Baloo Tammudu',}}><FontAwesomeIcon icon={faUserCircle} />Profile</NavLink>
-          <NavLink className="links" href="/login" style={{fontSize:"35px", fontFamily:'Baloo Tammudu',}}><FontAwesomeIcon icon={faSignInAlt} />Login</NavLink> 
-          <NavLink className="links" href="/signup"style={{fontSize:"35px", fontFamily:'Baloo Tammudu',}}><FontAwesomeIcon icon={faGlobeAmericas} />Signup</NavLink>
-        </NavWrapper>
+    <NavWrapper className="flex-column">
+      <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+        <NavLink
+          className="links"
+          href="/"
+          style={{ fontSize: "30px", fontFamily: "Baloo Tammudu" }}
+        >
+          <FontAwesomeIcon icon={faHome} />
+          About
+        </NavLink>
+      </motion.div>
+      <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+        <NavLink
+          className="links"
+          href="/profile"
+          style={{ fontSize: "30px", fontFamily: "Baloo Tammudu" }}
+        >
+          <FontAwesomeIcon icon={faUserCircle} />
+          Profile
+        </NavLink>
+      </motion.div>
+      <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+        <NavLink
+          className="links"
+          href="/login"
+          style={{ fontSize: "30px", fontFamily: "Baloo Tammudu" }}
+        >
+          <FontAwesomeIcon icon={faSignInAlt} />
+          Login
+        </NavLink>
+      </motion.div>
+      <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+        <NavLink
+          className="links"
+          href="/signup"
+          style={{ fontSize: "30px", fontFamily: "Baloo Tammudu" }}
+        >
+          <FontAwesomeIcon icon={faGlobeAmericas} />
+          Signup
+        </NavLink>
+      </motion.div>
+    </NavWrapper>
   );
 };
 
